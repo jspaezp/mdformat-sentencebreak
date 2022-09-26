89a25f81571b1bac6181a89c799eeca45e2e465f
J. Sebastian Paez <jspaezp@users.noreply.github.com>
2022-09-25 20:40:23 -0500

    version bump

--- mdformat_sentencebreak/__init__.py
+++ mdformat_sentencebreak/__init__.py
@@ -1,7 +1,5 @@
 """An mdformat plugin for..."""
 
-import importlib.metadata
-
-__version__ = importlib.metadata.version(__name__)
+__version__ = "0.2.0"
 
 from .plugin import RENDERERS, update_mdit  # noqa: E402, F401
